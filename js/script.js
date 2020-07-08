4b43ec6aa032838e0da82eefb3dd032946120edf
Patryk <Patrex97@gmail.com>
2020-07-08 15:00:25 +0200

    Desktop CSS update
    Changes:
    - One hue for all website
    - search input update (button, inside text)
    - add first css to footer

--- js/script.js
+++ js/script.js
@@ -7,3 +7,16 @@ activation.addEventListener('click', () => {
     menu.classList.toggle('hidden');
     console.log(menu);
 });
+
+// Inside text disapear in search input
+
+const input = document.querySelector('.search-input');
+const text = document.querySelector('.text-inside');
+
+input.addEventListener('focus', () => {
+    text.style.display = 'none';
+});
+
+input.addEventListener('blur', () => {
+    if (input.value === '') text.style.display = 'block';
+});
